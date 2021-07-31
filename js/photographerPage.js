b21311d37b16faa424bf0f1fdf0da2b9ab3a25b4
ghaithdiab <ghaith.diab@hotmail.com>
2021-07-31 11:38:55 +0200

    form fonctionnalités

--- js/photographerPage.js
+++ js/photographerPage.js
@@ -84,4 +84,28 @@ likebtn.forEach(element=>{
   })
 })
 price.innerText=`${photographer.price}€/jour`;
+/////////form
+const formModal=document.querySelector('.bground');
+const btnContact=document.querySelector(".btn");
+const btnClose=document.querySelector('.close-btn');
+btnContact.addEventListener('click',()=>{
+  formModal.style.display="block";
+})
+btnClose.addEventListener('click',()=>{
+  formModal.style.display="none";
+})
 
+const namePhotographer=document.querySelector('.name-cotact');
+namePhotographer.innerText=`Contactez-moi ${photographer.name}`;
+
+const allInput=document.querySelectorAll('input');
+const textMessage=document.querySelector("textarea");
+const btnSend=document.querySelector('.btn-submit');
+btnSend.addEventListener('click',(e)=>{
+  e.preventDefault();
+  console.log(`Prénom : ${allInput[0].value}`);
+  console.log(`Nom : ${allInput[1].value}`);
+  console.log(`Email : ${allInput[2].value}`);
+  console.log(`Message : ${textMessage.value}`);
+  formModal.style.display="none";
+})
